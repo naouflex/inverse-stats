6c9c166ecaff5fa6c7a8263c94710d27b45852a2
naouflex <naoufel.eth@gmail.com>
2023-10-12 14:04:28 +0200

    Clean blocks

--- blocks_daily.py
+++ blocks_daily.py
@@ -22,53 +22,7 @@ def get_rpc_table():
     except Exception as e:
         logger.error(f"Cannot get rpc table: {e}")
         return
-
-def update_block_table(db_url,table_name,end_date=None):
-
-    try:
-        # get block table to get the last date in the db
-        block_table = get_table(db_url,table_name)
-        
-        last_date = block_table['date'].max()
-
-        # convert unix timestamp to datetime
-        last_date = datetime.fromtimestamp(last_date)
-
-        # if lastdate is today we exit
-        if last_date.year == datetime.now().year and last_date.month == datetime.now().month and last_date.day == datetime.now().day:
-            logger.warning("Block table is up to date")
-            return
-        
-        last_date = last_date + timedelta(days=1)
-
-        if end_date is None:
-            end_date = datetime.now().strftime("%Y-%m-%d 00:00:00")
-        
-        web3_providers = get_rpc_table()
-
-        for i in range(len(web3_providers)):
-            w3 = Web3(Web3.HTTPProvider(web3_providers['rpc_url'][i]))
-            w3.middleware_onion.inject(geth_poa_middleware, layer=0)
-            chain_id = web3_providers['chain_id'][i]
-            chain_name = web3_providers['chain_name'][i]
-
-            chain_blocks = get_blocks_by_date_range(w3,last_date, end_date)
-            
-            if i==0:
-                chain_blocks = chain_blocks.rename(columns={'block_number': chain_name})
-                block_table = chain_blocks
-            else:
-                #rename block_number to chain_name_block_number and left merge to block_table
-                chain_blocks = chain_blocks.rename(columns={'block_number': chain_name})
-                block_table = pd.merge(block_table, chain_blocks, on='date', how='left')
-            logger.info(f"chain_id: {chain_id}, chain_name: {chain_name} processed")
-        block_table = pd.DataFrame(block_table)
-        update_table(db_url,table_name,block_table)
-        return
-    except Exception as e:
-        logger.error(f"Cannot update block table: {e}")
-        return
-    
+  
 def create_history():
     try:
         db_url = os.getenv('PROD_DB')
@@ -107,8 +61,58 @@ def create_history():
         return
 
 def update_history():
-    db_url = os.getenv('PROD_DB')
-    start_time = datetime.now()
-    #end_date = "2020-01-05 00:00:00"  
-    update_block_table(db_url,'blocks_daily')
-    logger.info(f"Update Block Table - Time elapsed: {datetime.now() - start_time}")
+
+    try:
+        start_time = datetime.now()
+
+        db_url = os.getenv('PROD_DB')
+        table_name = 'blocks_daily'
+        
+        end_date=None
+        #end_date = "2020-01-05 00:00:00"  
+        
+        # get block table to get the last date in the db
+        block_table = get_table(db_url,table_name)
+        
+        last_date = block_table['date'].max()
+
+        # convert unix timestamp to datetime
+        last_date = datetime.fromtimestamp(last_date)
+
+        # if lastdate is today we exit
+        if last_date.year == datetime.now().year and last_date.month == datetime.now().month and last_date.day == datetime.now().day:
+            logger.warning("Block table is up to date")
+            return
+        
+        last_date = last_date + timedelta(days=1)
+
+        if end_date is None:
+            end_date = datetime.now().strftime("%Y-%m-%d 00:00:00")
+        
+        web3_providers = get_rpc_table()
+
+        for i in range(len(web3_providers)):
+            w3 = Web3(Web3.HTTPProvider(web3_providers['rpc_url'][i]))
+            w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+            chain_id = web3_providers['chain_id'][i]
+            chain_name = web3_providers['chain_name'][i]
+
+            chain_blocks = get_blocks_by_date_range(w3,last_date, end_date)
+            
+            if i==0:
+                chain_blocks = chain_blocks.rename(columns={'block_number': chain_name})
+                block_table = chain_blocks
+            else:
+                #rename block_number to chain_name_block_number and left merge to block_table
+                chain_blocks = chain_blocks.rename(columns={'block_number': chain_name})
+                block_table = pd.merge(block_table, chain_blocks, on='date', how='left')
+            logger.info(f"chain_id: {chain_id}, chain_name: {chain_name} processed")
+        block_table = pd.DataFrame(block_table)
+        update_table(db_url,table_name,block_table)
+        
+        logger.info(f"Update Block Table - Time elapsed: {datetime.now() - start_time}")
+        
+        return
+    except Exception as e:
+        logger.error(f"Cannot update block table: {e}")
+        return
