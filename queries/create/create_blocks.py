8e7f809320eb85ce073ea6dc96904bb455ea6757
naouflex <naoufel.eth@gmail.com>
2023-10-27 11:01:25 +0200

    add

---
+++ queries/create/create_blocks.py
@@ -0,0 +1,18 @@
+import traceback
+import os 
+from dotenv import load_dotenv
+from scripts.blocks_daily import create_current, create_history
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[logging.StreamHandler()])
+
+try:
+    db_url = os.getenv('PROD_DB')
+    table_name = 'blocks_daily'
+
+    create_history(db_url,table_name)
+
+except Exception as e:
+    print(traceback.format_exc())
